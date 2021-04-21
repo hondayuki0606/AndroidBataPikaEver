39effe4c53b8e637960cd68277dc69cdc5d4ea29
本田尚行 <hondayuki0606@yahoo.co.jp>
2021-04-21 23:10:57 +0900

    TorchOnService の実装

--- app/src/main/java/com/example/hondanaoyuki/batapikaever/TorchOnService.kt
+++ app/src/main/java/com/example/hondanaoyuki/batapikaever/TorchOnService.kt
@@ -1,4 +1,25 @@
 package com.example.hondanaoyuki.batapikaever
 
-class TorchOnService {
+import android.app.Service
+import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.os.IBinder
+
+class TorchOnService : Service(),SensorEventListener {
+    private val threshold: Float = 10f
+    private val oldValue: Float = 0f
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("Not yet implemented")
+    }
 }
